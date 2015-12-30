55a29e570756c3d0535957197f3d07d3ff3cd4c4
Zeuxis Lo <seekstudio@gmail.com>
2015-12-30 15:53:14 +0800

    move FreeProxyLists test case to self test file

---
+++ provider/free_proxy_lists_test.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+    "testing"
+)
+
+func TestNameMethod(t *testing.T) {
+    proxyProvider := &FreeProxyLists{}
+
+    if proxyProvider.Name() != "Free Proxy Lists" {
+        t.Errorf("Expected name should be Free Proxy Lists, current is %s", proxyProvider.Name())
+    }
+}
+
+func TestFetchMethod(t *testing.T) {
+    proxyProvider := &FreeProxyLists{}
+    resp, err     := proxyProvider.Fetch()
+
+    if err != nil {
+        t.Error(err)
+    }
+
+    if resp.StatusCode != 200 {
+        t.Errorf("Expected StatusCode=200, current StatusCode=%v", resp.StatusCode)
+    }
+}
+
+func TestResultMethod(t *testing.T) {
+    proxyProvider := &FreeProxyLists{}
+    resp, _       := proxyProvider.Fetch()
+
+    proxyList, err := proxyProvider.Result(resp)
+
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(proxyList) != 50 {
+        t.Error("Expected proxy list should be equals 50 rows")
+    }
+
+    proxy := proxyList[0]
+
+    if proxy.Ip == "" || proxy.Port == "" || proxy.Protocol == "" {
+        t.Error("The first proxy should not be empty in IP, Port and Protocol fields")
+    }
+}
