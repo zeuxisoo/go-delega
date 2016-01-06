66393bb7ef953841a87e3f528159a8bf5a9ad3b7
Zeuxis Lo <seekstudio@gmail.com>
2016-01-06 14:05:52 +0800

    finish result method in xicidaili provider

--- provider/xi_ci_dai_li.go
+++ provider/xi_ci_dai_li.go
@@ -2,6 +2,7 @@ package provider
 
 import (
     "github.com/parnurzeal/gorequest"
+    "github.com/PuerkitoBio/goquery"
 
     "github.com/zeuxisoo/go-delega/contract"
 
@@ -19,7 +20,7 @@ func(this *XiCiDaiLi) Fetch() (*http.Response, error) {
     request := gorequest.New()
 
     resp, _, errs := request.
-        Get("http://www.xicidaili.com/").
+        Get("http://www.xicidaili.com/wn/").
         Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36").
         End()
 
@@ -31,5 +32,30 @@ func(this *XiCiDaiLi) Fetch() (*http.Response, error) {
 }
 
 func (this *XiCiDaiLi) Result(response *http.Response) ([]contract.ProxyList, error) {
-    return nil, nil
+    doc, err := goquery.NewDocumentFromResponse(response)
+
+    if err != nil {
+        return nil, err
+    }
+
+    var proxyList []contract.ProxyList
+
+    proxyTable := doc.Find("table#ip_list")
+    proxyRows  := proxyTable.Find("tr:nth-child(n+2)")
+
+    proxyRows.Each(func(i int, s *goquery.Selection) {
+        ip         := s.Find("td:nth-child(3)").Text()
+        port       := s.Find("td:nth-child(4)").Text()
+        protocol   := s.Find("td:nth-child(7)").Text()
+
+        if ip != "" && port != "" {
+            proxyList = append(proxyList, contract.ProxyList{
+                Ip      : ip,
+                Port    : port,
+                Protocol: protocol,
+            })
+        }
+    })
+
+    return proxyList, nil
 }
--- provider/xi_ci_dai_li_test.go
+++ provider/xi_ci_dai_li_test.go
@@ -24,3 +24,24 @@ func TestFetchMethod(t *testing.T) {
         t.Errorf("Expected StatusCode=200, current StatusCode=%v", resp.StatusCode)
     }
 }
+
+func TestResultMethod(t *testing.T) {
+    proxyProvider := &XiCiDaiLi{}
+    resp, _       := proxyProvider.Fetch()
+
+    proxyList, err := proxyProvider.Result(resp)
+
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(proxyList) != 100 {
+        t.Error("Expected proxy list should be equals 100 rows")
+    }
+
+    proxy := proxyList[0]
+
+    if proxy.Ip == "" || proxy.Port == "" || proxy.Protocol == "" {
+        t.Error("The first proxy should not be empty in IP, Port and Protocol fields")
+    }
+}
