ba38187241dd289cc5a94f1b5bced33e2ab49173
Zeuxis Lo <seekstudio@gmail.com>
2016-01-06 16:51:26 +0800

    add comments on delega.go

--- delega.go
+++ delega.go
@@ -7,11 +7,13 @@ import (
     "github.com/zeuxisoo/go-delega/provider"
 )
 
+// Supported provider names
 const (
     FreeProxyLists  = iota
     XiCiDaiLi
 )
 
+// Return created provider by pass provider name
 func Create(providerType int) (contract.Provider, error) {
     switch(providerType) {
         case FreeProxyLists:
