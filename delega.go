b5e75c6af07a9cb260037c2ec45febcd44854e9e
Zeuxis Lo <seekstudio@gmail.com>
2016-01-06 14:37:05 +0800

    add new provider const XiCiDaiLi into delega and test case

--- delega.go
+++ delega.go
@@ -9,7 +9,7 @@ import (
 
 const (
     FreeProxyLists  = iota
-    OtherProxyLists
+    XiCiDaiLi
 )
 
 type Delega struct {
@@ -20,6 +20,8 @@ func Create(providerType int) (contract.Provider, error) {
     switch(providerType) {
         case FreeProxyLists:
             return new(provider.FreeProxyLists), nil
+        case XiCiDaiLi:
+            return new(provider.XiCiDaiLi), nil
         default:
             return nil, errors.New("Invalid provider type")
     }
--- delega_test.go
+++ delega_test.go
@@ -4,7 +4,7 @@ import (
     "testing"
 )
 
-func TestCreateMethod(t *testing.T) {
+func TestCreateMethodOnFreeProxyLists(t *testing.T) {
     provider, err := Create(FreeProxyLists)
 
     if err != nil {
@@ -12,6 +12,19 @@ func TestCreateMethod(t *testing.T) {
     }
 
     if provider.Name() != "Free Proxy Lists" {
+        t.Error("Expected provider name is not equals.")
+    }
+}
+
+
+func TestCreateMethodOnXiCiDaiLi(t *testing.T) {
+    provider, err := Create(XiCiDaiLi)
+
+    if err != nil {
+        t.Error(err)
+    }
+
+    if provider.Name() != "Xi Ci Dai Li" {
         t.Error("Expected provider name is not equals.")
     }
 }
