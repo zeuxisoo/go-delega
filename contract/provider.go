30f3b7416c99e03dd11afe99fa701f92aed46d72
Zeuxis Lo <seekstudio@gmail.com>
2016-01-07 16:24:36 +0800

    add comment to provider.go

--- contract/provider.go
+++ contract/provider.go
@@ -1,15 +1,18 @@
+// Provider contract is a set of interfaces or structs that the services used by the library
 package contract
 
 import (
     "net/http"
 )
 
+// Provider is an interface that make sure all providers are contain same methods
 type Provider interface {
     Name() string
     Fetch() (*http.Response, error)
     Result(*http.Response) ([]ProxyList, error)
 }
 
+// ProxyList represents a collection of proxy matching defined format
 type ProxyList struct {
     Ip          string
     Port        string
